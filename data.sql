57b74ce3b45cd8c95a776bd10879edb03f718b7e
Vicky Lin <jiajun_work@126.com>
2020-06-26 11:45:46 +0800

    finish multi-table-query homework

--- data.sql
+++ data.sql
@@ -1,40 +1,45 @@
-create table student(id int(10),name varchar(10),age datetime,sex varchar(10));
+# 1.Query the existence of 1 course 查询同时存在1课程和2课程的情况
+select s.name,sc.courseId,sc.score,s.id from student_course sc,student s 
+where sc.courseId in(select courseId from student_course where sc.courseId = 1 or sc.courseId = 2)
+and sc.studentId = s.id
 
-insert into student values(1 , 'Cotton' , '1990-01-01' , 'male');
-insert into student values(2 , 'Couch' , '1990-12-21' , 'male');
-insert into student values(3 , 'Courtney' , '1990-05-20' , 'male');
-insert into student values(4 , 'Imogene' , '1990-08-06' , 'male');
-insert into student values(5 , 'Cottrell' , '1991-12-01' , 'female');
-insert into student values(6 , 'Abelard' , '1992-03-01' , 'female');
-insert into student values(7 , 'Deane' , '1989-07-01' , 'female');
-insert into student values(8 , 'Dawson' , '1990-01-20' , 'female');
+# 2.Query the presence of both 1 and 2 courses
+select s.name,sc.courseId,sc.score,s.id from student_course sc,student s 
+where sc.courseId in(select courseId from student_course where sc.courseId = 1 or sc.courseId = 2)
+and sc.studentId = s.id;
 
-create table course(id int(10),name varchar(10),teacherId int(10));
-insert into course values(1 , 'Language' , 2);
-insert into course values(2 , 'Math' , 1);
-insert into course values(3 , 'English' , 3);
+# 3.Query the student number and student name and average score of students whose average score is 60 or higher.
+select sc.studentId,s.name,round(avg(sc.score),2) averageScore from student_course sc left join student s
+on sc.studentId = s.id
+group by sc.studentId,s.name having round(avg(sc.score),2)>=60;
 
-create table teacher(id int(10),name varchar(10));
-insert into teacher values(1 , 'Andrew');
-insert into teacher values(2 , 'Denny');
-insert into teacher values(3 , 'Barry');
+# 4.Query the SQL statement of student information that does not have grades in the student_course table
+select * from student where id not in(select studentId from student_course);
 
-create table student_course(studentId int(10),courseId varchar(10),score decimal(18,1));
-insert into student_course values(1 , 1 , 80);
-insert into student_course values(1 , 2 , 90);
-insert into student_course values(1 , 3 , 99);
-insert into student_course values(2 , 1 , 70);
-insert into student_course values(2 , 2 , 60);
-insert into student_course values(2 , 3 , 80);
-insert into student_course values(3 , 1 , 80);
-insert into student_course values(3 , 2 , 80);
-insert into student_course values(3 , 3 , 80);
-insert into student_course values(4 , 1 , 50);
-insert into student_course values(4 , 2 , 30);
-insert into student_course values(4 , 3 , 20);
-insert into student_course values(5 , 1 , 76);
-insert into student_course values(5 , 2 , 87);
-insert into student_course values(6 , 1 , 31);
-insert into student_course values(6 , 3 , 34);
-insert into student_course values(7 , 2 , 89);
-insert into student_course values(7 , 3 , 98);
+# 5.Query all SQL with grades
+select s.id,s.name,s.age,s.sex from student_course sc left join student s on sc.studentId = s.id
+group by sc.studentId,s.id,s.name,s.age,s.sex;
+
+# 6.Inquire about the information of classmates who have numbered 1 and also studied the course numbered 2
+select s.id,s.name,s.age,s.sex,stu.c1,stu.c2
+from (select t1.studentId sId,t1.courseId c1,t2.courseId c2 from
+	(select s1.studentId,s1.courseId from student_course s1 where s1.courseId=1) t1,
+	(select s2.studentId,s2.courseId from student_course s2 where s2.courseId=2) t2
+    where t1.studentId = t2.studentId
+    )stu,student s
+where stu.sId = s.id;
+
+# 7.Retrieve 1 student score with less than 60 scores in descending order
+select s.name,s.age,s.id,s.sex,sc.score,sc.courseId
+from student_course sc left join student s on sc.studentId = s.id
+where sc.courseId = 1 and sc.score<60
+order by sc.score desc;
+
+# 8.Query the average grade of each course. The results are ranked in descending order of average grade. When the average grades are the same, they are sorted in ascending order by course number.
+select c.id,c.name,round(avg(sc.score),2) avgScore from student_course sc left join course c on sc.courseId = c.id
+group by c.id,c.name 
+order by round(avg(sc.score),2) desc,c.id asc;
+
+# 9.Query the name and score of a student whose course name is "Math" and whose score is less than 60
+select s.name,sc.score from student s,student_course sc,course c
+where c.name='Math' and c.id = sc.courseId and s.id = sc.studentId and sc.score < 60;
